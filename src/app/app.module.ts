9a65cf037b74e3d74e6a27e72b5d36d297d903d4
cwallace033 <145495577+cwallace033@users.noreply.github.com>
2025-07-10 06:56:48 -0600

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CardListComponent } from './components/card-list/card-list.component';
-import { CardFormComponent } from './components/card-form/card-form.component';
+import { CardListComponent } from './components/cards/card-list/card-list.component';
+import { CardFormComponent } from './components/cards/card-form/card-form.component';
+import { DeskListComponent } from './components/deck_building/desk-list/desk-list.component';
+import { DeskFormComponent } from './components/deck_building/desk-form/desk-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardListComponent,
-    CardFormComponent
+    CardFormComponent,
+    DeskListComponent,
+    DeskFormComponent
   ],
   imports: [
     BrowserModule,
